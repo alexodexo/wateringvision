bddcdfd9a946578b97cb4f1c26886b6902644773
alexodexo <102768093+alexodexo@users.noreply.github.com>
2024-03-10 02:47:24 +0100

    Config Zisternenmodul

---
+++ modul/ultra_settings.h
@@ -0,0 +1,11 @@
+// secrets.h
+#define WIFI_SSID "Name"
+#define WIFI_PASSWORD "PW"
+#define clientname "kopenhagen"
+#define mqtt_password "mqtt_pw"
+
+//Distanz in cm zwischen Ultraschallsensor und Zisternenboden
+#define maxDepth 300
+
+// Maximales Volumen der Zisterne in Litern
+#define maxVolume 3500
