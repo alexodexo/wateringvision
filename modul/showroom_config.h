58573a65d8257ae1a69b62c8b2a017526754b104
alexodexo <102768093+alexodexo@users.noreply.github.com>
2024-03-18 00:14:20 +0100

    added timeserver to config

---
+++ modul/showroom_config.h
@@ -0,0 +1,34 @@
+// WLAN
+#define WIFI_SSID "DEINWLANNAME"
+#define WIFI_PASSWORD "DEINWLANPW"
+
+//NTP Time Server
+#define ntpServer "time.cloudflare.com"
+
+//MQTT Config
+#define clientname "berlin"
+#define mqtt_password "DEINMQTTPW"
+#define mqtt_server "wateringvision.de"
+#define mqtt_port 1883
+
+//MQTT Topics
+#define feedback_topic "/feedback/berlin"
+#define valve_topic "/operateValve/berlin"
+#define request_topic "/requestSensor/berlin"
+#define response_topic "/responseSensor/berlin"
+#define notaus_topic "/notaus/berlin"
+
+//Ultraschall Pins
+#define echoPin 17
+#define trigPin 16
+
+//Feuchtigkeitspins
+#define capacitivePin 35
+#define electricPin 34
+
+//I2C
+#define SDA_PIN 0
+#define SCL_PIN 4
+
+//Valve
+#define relaisPin 27
