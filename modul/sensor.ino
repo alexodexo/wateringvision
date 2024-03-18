70437ee92e9d7c0add06d94a4c0e6be5a7590fb4
alexodexo <102768093+alexodexo@users.noreply.github.com>
2024-03-18 01:11:27 +0100

    added timerserver function

--- modul/sensor.ino
+++ modul/sensor.ino
@@ -1,37 +1,44 @@
-#include "secrets.h"
-#include <WiFi.h>
+#include "sensorik_config.h"
 
+#include <WiFi.h>
 WiFiClient espClient;
 
+#include "time.h"
+const long gmtOffset_sec = 3600;   // GMT +1: 3600 Sekunden Offset
+const int daylightOffset_sec = 0;  // Lass die Anpassung automatisch erfolgen
+struct tm timeinfo;                // Deklariere timeinfo als globale Variable
+String hhmm;
+
 #include <PubSubClient.h>
-const char* mqtt_server = "wateringvision.de";
-const int mqtt_port = 1883;
 PubSubClient client(espClient);
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-#define capacitivePin 35
-int capacitiveValue;
+long duration;  //Ultraschall, of sound wave travel
+int distance;   //Ultraschall, for the distance measurement
 
-#define electricPin 34
+int capacitiveValue;
+int capacitivePercent;
+
 int electricValue;
 int elecPercent;
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#define SDA_PIN 0
-#define SCL_PIN 4
 Adafruit_BME280 bme;  // Erstelle ein BME280 Objekt
 int temperature;
 int pressure;
 int humidity;
 
+#include <ArduinoJson.h>
+#include <Arduino.h>
 
-unsigned long previousMillis = 0; //Sensordisplay Startwert
 
 void setup() {
+  pinMode(trigPin, OUTPUT);
+
   Serial.begin(115200);
   Wire.begin(SDA_PIN, SCL_PIN);
   lcd.init();
@@ -43,7 +50,8 @@ void setup() {
   delay(1000);
 
   setup_wifi();
- 
+  initializeTime();
+
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
@@ -59,22 +67,38 @@ void loop() {
     reconnect();
   }
   client.loop();
-  sensorDisplay();
+
+  updateDisplayData();
 }
 
-void sensorDisplay(){
-  lcd.setCursor(0, 0);
-  lcd.print("Feuchtigkeitwert");
-  if (millis() - previousMillis >= 1500) {
-    previousMillis += 1500; // Aktualisiere die Zeit für das nächste Intervall
-    int percent = electricMessure();
-    String percentString = String(percent) + "%";
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-    lcd.setCursor(6, 1);
-    lcd.print(percentString);
+
+
+void updateDisplayData() {
+  static unsigned long lastUpdate = 0;
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - lastUpdate >= 2000) {  // Prüfe, ob 2 Sekunden vergangen sind
+    lastUpdate = currentMillis;              // Aktualisiere die letzte Aktualisierungszeit
+
+    messure();  // Aktualisiere Messwerte
+    lcd.clear();
+    // Aktualisiere die obere Zeile mit Bodenfeuchtigkeit
+    char topLine[11];  // Char-Array für "Soil: 100%"
+    snprintf(topLine, sizeof(topLine), "Soil: %d%%", elecPercent);
+    lcd.setCursor(0, 0);  // Setze den Cursor auf den Anfang der ersten Zeile
+    lcd.print(topLine);   // Drucke die Bodenfeuchtigkeit
+
+    printHHMM();  // Aktualisiere die Zeit am Ende der ersten Zeile, die Funktion kümmert sich um die korrekte Position
+
+    // Aktualisiere die untere Zeile mit Temperatur, Luftdruck und Luftfeuchtigkeit
+    char bottomLine[17];  // Char-Array für die untere Zeile
+    snprintf(bottomLine, sizeof(bottomLine), "%dC %dhPa %d%%", temperature, pressure, humidity);
+    lcd.setCursor(0, 1);    // Setze den Cursor auf den Anfang der zweiten Zeile
+    lcd.print(bottomLine);  // Drucke die gesammelten Messwerte
   }
 }
+
+
 
 void callback(char* topic, byte* message, unsigned int length) {
   lcd.setCursor(0, 0);
@@ -93,35 +117,42 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Payloud on the toppic: ");
   Serial.println(msg);
 
-  handleCallSensor();
-}
 
-void handleCallSensor() {
-  messure();
-
-  char sensorDataString[60];
-  createSensorDataString(0, elecPercent, capacitiveValue, temperature, pressure, humidity, sensorDataString, sizeof(sensorDataString));
-  Serial.println(sensorDataString);
-
-  // Veröffentliche die Nachricht auf "berlin/setSensor"
-  if (client.publish("/responseSensor/berlin", sensorDataString)) {
-    Serial.println("Sensorwert erfolgreich veröffentlicht!");
-    client.publish("/feedback/berlin", "Sensordaten erfolgreich veröffentlicht");
-
+  if (strcmp(topic, request_topic) == 0) {
+    handleCallSensor();
   } else {
-    Serial.println("Fehler beim Veröffentlichen des Sensorwerts.");
-    client.publish("/feedback/berlin", "Fehler beim Übermitteln der Sensordaten");
+    Serial.print("Unbekanntes Topic: ");
+    Serial.println(topic);
+    client.publish(feedback_topic, "Ungültiger Befehl übermittelt.");
   }
 }
 
 
 
+
+void handleCallSensor() {
+  messure();
+
+  char sensorDataString[60];
+  createSensorDataString(distance, electricValue, capacitiveValue, temperature, pressure, humidity, sensorDataString, sizeof(sensorDataString));
+  Serial.println(sensorDataString);
+
+  if (client.publish(response_topic, sensorDataString)) {
+    Serial.println("Sensorwert erfolgreich veröffentlicht!");
+    client.publish(feedback_topic, "Sensordaten erfolgreich veröffentlicht");
+
+  } else {
+    Serial.println("Fehler beim Veröffentlichen des Sensorwerts.");
+    client.publish(feedback_topic, "Fehler beim Übermitteln der Sensordaten");
+  }
+}
+
 void messure() {
   bmeMesure();
+  ultrasonicMessure();
   electricMessure();
   capacitiveMesure();
 }
-
 
 void createSensorDataString(int ultra, int elec, int cap, int temp, int pres, int humi, char* dataString, int maxLength) {
   snprintf(dataString, maxLength, "ultra:%d;elec:%d;cap:%d;temp:%d;pres:%d;humi:%d", ultra, elec, cap, temp, pres, humi);
@@ -140,7 +171,9 @@ void reconnect() {
       lcd.setCursor(0, 1);
       lcd.print("   connected    ");
       Serial.println("Connected to MQTT broker");
-      client.subscribe("/requestSensor/berlin", 1);
+      client.subscribe(request_topic, 1);
+
+
     } else {
       Serial.print("Failed, rc=");
       int clientState = client.state();
@@ -154,8 +187,6 @@ void reconnect() {
     }
   }
 }
-
-
 
 void setup_wifi() {
   delay(10);
@@ -179,6 +210,48 @@ void setup_wifi() {
   delay(1000);
 }
 
+void initializeTime() {
+  // Konfiguriere Zeitserver und Zeitzone
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  lcd.setCursor(0, 0);
+  lcd.print("   Timeserver   ");
+  lcd.setCursor(0, 1);
+  lcd.print(" not connected  ");
+
+
+  Serial.println("Warte auf Zeit synchronisation...");
+  while (!getLocalTime(&timeinfo)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.print("Zeit synchronisiert: ");
+  Serial.println(getHHMM());
+
+
+  lcd.setCursor(0, 1);
+  lcd.print("   connected    ");
+}
+
+String getHHMM() {
+  if (!getLocalTime(&timeinfo)) {
+    return "Fehler";  // Falls keine Zeit abgerufen werden kann
+  }
+  char timeString[6];  // HH:MM + Null-Terminator
+  strftime(timeString, sizeof(timeString), "%H:%M", &timeinfo);
+  return String(timeString);  // Konvertiere den char-Array in ein String-Objekt und gebe es zurück
+}
+
+void printHHMM() {
+  Serial.println("Zeit wird auf dem Display aktualisiert");
+  hhmm = getHHMM();
+  lcd.setCursor(11, 0);
+  lcd.print(hhmm);
+  Serial.print("Aktuelle Zeit: ");
+  Serial.println(hhmm);
+}
+
+
+
 void bmeMesure() {
   Serial.print("Temperature = ");
   temperature = bme.readTemperature();
@@ -196,6 +269,24 @@ void bmeMesure() {
   Serial.println(" %");
 }
 
+long ultrasonicMessure() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);  //
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
+  Serial.print("Distance in cm: ");
+  Serial.println(distance);
+  return distance;
+}
+
+
 int electricMessure() {
   electricValue = analogRead(electricPin);
   elecPercent = map(electricValue, 4095, 1000, 0, 100);
@@ -205,9 +296,11 @@ int electricMessure() {
 }
 
 
+
 int capacitiveMesure() {
   capacitiveValue = analogRead(capacitivePin);
-  Serial.print("Capacitive Value: ");
-  Serial.println(capacitiveValue);
-  return capacitiveValue;
+  capacitivePercent = map(capacitiveValue, 3000, 2100, 0, 100);
+  Serial.print("Moisture % (cap): ");
+  Serial.println(capacitivePercent);
+  return capacitivePercent;
 }
