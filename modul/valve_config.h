cc5b675d8ba8a0fdc0131a9acf478e9e4f1f2189
alexodexo <102768093+alexodexo@users.noreply.github.com>
2024-03-18 01:04:57 +0100

    added timeserver to config

---
+++ modul/valve_config.h
@@ -0,0 +1,24 @@
+// WLAN
+#define WIFI_SSID "DEINWLANNAME"
+#define WIFI_PASSWORD "DEINWLANPW"
+
+//NTP Time Server
+#define ntpServer "time.cloudflare.com"
+
+//MQTT Config
+#define clientname "kopenhagen"
+#define mqtt_password "DEINMQTTPW"
+#define mqtt_server "wateringvision.de"
+#define mqtt_port 1883
+
+//MQTT Topics
+#define feedback_topic "/feedback/kopenhagen"
+#define valve_topic "/operateValve/kopenhagen"
+#define notaus_topic "/notaus/kopenhagen"
+
+//I2C
+#define SDA_PIN 0
+#define SCL_PIN 4
+
+//Valve
+#define relaisPin 27
